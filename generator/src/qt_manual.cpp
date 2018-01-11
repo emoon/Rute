e3d4c8d67aa2977cc692b817efeba3085e3b37b8
Daniel Collin <daniel@collin.com>
2018-01-11 21:45:44 +0100

    Use dark colors by default

--- generator/src/qt_manual.cpp
+++ generator/src/qt_manual.cpp
@@ -1,9 +1,35 @@
 
+#include <QStyleFactory>
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static struct PUApplication create_application(struct PUBase* priv_data) {
     static int argc = 0;
     QApplication* qt_obj = new QApplication(argc, 0);
+
+    QCoreApplication::setOrganizationName(QStringLiteral("TBL"));
+    QCoreApplication::setOrganizationDomain(QStringLiteral("tbl.org"));
+
+    qt_obj->setStyle(QStyleFactory::create(QStringLiteral("Fusion")));
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+    darkPalette.setColor(QPalette::WindowText, QColor(170,170,170));
+    darkPalette.setColor(QPalette::Text, QColor(170,170,170));
+    darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+    darkPalette.setColor(QPalette::Highlight, QColor(50, 60, 70));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    qt_obj->setPalette(darkPalette);
+
     struct PUApplication ctl;
     ctl.funcs = &s_application_funcs;
     ctl.priv_data = (struct PUBase*)qt_obj;
