1a328c36cc81fd5880c3e3f126503c6f80a849c0
Daniel Collin <daniel@collin.com>
2018-08-12 18:37:56 +0200

    Cleanup for Rust gen + added tests

--- rute_gen/src/rust_gen.rs
+++ rute_gen/src/rust_gen.rs
@@ -3,6 +3,7 @@ use std::io::{BufWriter, Write};
 use std::fs::File;
 use api_parser::*;
 use heck::{CamelCase, SnakeCase};
+//use std::fmt::Write;
 
 pub struct RustGenerator;
 
@@ -61,57 +62,82 @@ fn generate_structs<W: Write>(f: &mut W, api_def: &ApiDef) -> io::Result<()> {
 }
 
 ///
+/// Takes in a varibale and generates a Rust function variable argument
+///
+fn generate_arg_type(dest: &mut String, var: &Variable) {
+    dest.clear();
+
+    match var.vtype {
+        VariableType::None => dest.push_str("<None>"),
+        VariableType::SelfType => dest.push_str("&self"),
+        VariableType::Primitive => dest.push_str(&var.type_name),
+        VariableType::Enum => dest.push_str(&var.type_name),
+        VariableType::Regular => {
+            dest.push('&');
+            dest.push_str(&var.type_name)
+        },
+        VariableType::Reference => {
+            dest.push('&');
+            dest.push_str(&var.type_name)
+        },
+        VariableType::Optional => {
+            if var.type_name == "String" {
+                dest.push_str("Option<&str>")
+            } else {
+                dest.push_str("Option<");
+                dest.push_str(&var.type_name);
+                dest.push('>');
+            }
+        }
+    }
+}
+
+///
 /// Generate a function implementation
 ///
 fn generate_func_impl(func: &Function) -> String  {
+    let mut temp_str = String::with_capacity(128);
 	let mut func_imp = String::with_capacity(128);
 
 	func_imp.push_str("(&self");
 
 	for arg in &func.function_args[1..] {
-		let tname = match arg.vtype {
-			VariableType::Regular => {
-				format!("&{}", arg.type_name)
-			}
+        generate_arg_type(&mut temp_str, &arg);
 
-			VariableType::Primitive => {
-				arg.type_name.to_owned()
-			}
+        func_imp.push_str(", ");
+        func_imp.push_str(&arg.name);
+        func_imp.push_str(": ");
 
-			VariableType::Optional => {
-				if arg.type_name == "String" {
-					"Option<&str>".to_owned()
-				} else {
-					format!("Option<{}>", arg.type_name)
-				}
-			},
-
-			VariableType::Enum => {
-				arg.type_name.to_owned()
-			}
-
-			VariableType::Reference => {
-				format!("&{}", arg.type_name)
-			}
-
-			_ => "<illegal>".to_owned(),
-		};
-
-		if arg.array {
-			func_imp.push_str(&format!(", {}:& [{}]", arg.name, tname));
-		} else {
-			func_imp.push_str(&format!(", {}: {}", arg.name, tname));
-		}
+        if arg.array {
+            func_imp.push_str("&[");
+            func_imp.push_str(&temp_str);
+            func_imp.push(']');
+        } else {
+            func_imp.push_str(&temp_str);
+        }
 	}
 
-	func_imp.push_str(")");
+	func_imp.push(')');
 
 	//
 	// If we don't have any return value we alwayes return self
 	//
 	if func.return_val.is_none() {
 		func_imp.push_str(" -> &Self<'a>");
-	}
+	} else {
+	    let ret_val = func.return_val.as_ref().unwrap();
+
+        generate_arg_type(&mut temp_str, ret_val);
+		func_imp.push_str(" -> ");
+
+        if ret_val.array {
+		    func_imp.push_str("RefArray<");
+            func_imp.push_str(&temp_str);
+		    func_imp.push_str(", WrapperRcOwn>");
+        } else {
+            func_imp.push_str(&temp_str);
+        }
+    }
 
 	func_imp
 }
@@ -233,17 +259,12 @@ mod tests {
 	}
 
 	//
-	// Test function def with one primitive 
+	// Test function def with one primitive
 	//
 	#[test]
 	fn test_function_one_primitive() {
         let mut func = build_default_func();
-        func.function_args.push(Variable {
-			name: "foo".to_owned(),
-			type_name: "i32".to_owned(),
-			vtype: VariableType::Primitive,
-			.. Variable::default()
-		});
+        func.function_args.push(Variable { name: "foo".to_owned(), type_name: "i32".to_owned(), vtype: VariableType::Primitive, .. Variable::default() });
 
 		let func_impl = generate_func_impl(&func);
         assert_eq!(func_impl, "(&self, foo: i32) -> &Self<'a>");
@@ -255,40 +276,59 @@ mod tests {
 	#[test]
 	fn test_function_two_primitive() {
         let mut func = build_default_func();
-        func.function_args.push(Variable {
-			name: "width".to_owned(),
-			type_name: "i32".to_owned(),
-			vtype: VariableType::Primitive,
-			.. Variable::default()
-		});
-        func.function_args.push(Variable {
-			name: "height".to_owned(),
-			type_name: "f32".to_owned(),
-			vtype: VariableType::Primitive,
-			.. Variable::default()
-		});
+        func.function_args.push(Variable { name: "width".to_owned(), type_name: "i32".to_owned(), vtype: VariableType::Primitive, .. Variable::default() });
+        func.function_args.push(Variable { name: "height".to_owned(), type_name: "f32".to_owned(), vtype: VariableType::Primitive, .. Variable::default() });
 
 		let func_impl = generate_func_impl(&func);
         assert_eq!(func_impl, "(&self, width: i32, height: f32) -> &Self<'a>");
 	}
 
 	//
-	// Test function def with self and primitive return 
+	// Test function def with self and primitive return
 	//
-	/*
 	#[test]
-	fn test_function_two_primitive() {
+	fn test_function_primitive_return() {
         let mut func = build_default_func();
-        func.return_val = Some(Variable {
-			type_name: "i32".to_owned(),
-			vtype: VariableType::Primitive,
-			.. Variable::default()
-		});
+        func.return_val = Some(Variable { type_name: "i32".to_owned(), vtype: VariableType::Primitive, .. Variable::default() });
 
 		let func_impl = generate_func_impl(&func);
         assert_eq!(func_impl, "(&self) -> i32");
 	}
-	*/
 
+	//
+	// Test function def with self and optional return
+	//
+	#[test]
+	fn test_function_primitive_optional_return() {
+        let mut func = build_default_func();
+        func.return_val = Some(Variable { type_name: "f32".to_owned(), vtype: VariableType::Optional, .. Variable::default() });
+
+		let func_impl = generate_func_impl(&func);
+        assert_eq!(func_impl, "(&self) -> Option<f32>");
+	}
+
+	//
+	// Test function def with self and array return
+	//
+	#[test]
+	fn test_function_primitive_array_return() {
+        let mut func = build_default_func();
+        func.return_val = Some(Variable { type_name: "f32".to_owned(), vtype: VariableType::Primitive, array: true, .. Variable::default()});
+
+		let func_impl = generate_func_impl(&func);
+        assert_eq!(func_impl, "(&self) -> RefArray<f32, WrapperRcOwn>");
+	}
+
+	//
+	// Test function def with self, array input
+	//
+	#[test]
+	fn test_function_array_input() {
+        let mut func = build_default_func();
+        func.function_args.push(Variable { name: "width".to_owned(), type_name: "i32".to_owned(), vtype: VariableType::Primitive, array: true, .. Variable::default() });
+
+		let func_impl = generate_func_impl(&func);
+        assert_eq!(func_impl, "(&self, width: &[i32]) -> &Self<'a>");
+	}
 }
 
