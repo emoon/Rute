4f52e15ae2c23eb94815271bd8f2142b155f353d
Daniel Collin <daniel@collin.com>
2018-08-13 13:33:32 +0200

    Added TypeHandlers for Rust gen
    * Removed "hack" inside the argument generator as it now uses the TypeHandlers instead which make the implementation a bit cleaner
    * Updated unit-tests with the new code
    * Added unit-test to check if a String gets replace with str as expected for input parameters

--- rute_gen/src/rust_gen.rs
+++ rute_gen/src/rust_gen.rs
@@ -3,6 +3,8 @@ use std::io::{BufWriter, Write};
 use std::fs::File;
 use api_parser::*;
 use heck::{CamelCase, SnakeCase};
+use std::borrow::Cow;
+use std::collections::HashMap;
 //use std::fmt::Write;
 
 pub struct RustGenerator;
@@ -31,6 +33,55 @@ impl<'a> Rute<'a> {
         }
     }
 ";
+
+///
+/// Trait for handling different types that needs to be overriden
+///
+trait TypeHandlerTrait {
+    fn replace_type(&self, arg: &Variable, _is_return_value: bool) -> Cow<str> { 
+        arg.type_name.clone().into()
+    }
+
+    fn gen_body_return(&self, _varible: &Variable) -> Cow<str> {
+        "".into()
+    }
+
+    fn gen_body(&self, _arg: &str, _index: usize) -> (Cow<str>, Cow<str>);
+}
+
+///
+/// Keeps track of all the type handlers and when to apply them
+///
+type TypeHandler = HashMap<&'static str, Box<TypeHandlerTrait>>;
+
+///
+/// String type handler
+///
+#[derive(Clone)]
+struct StringTypeHandler;
+
+///
+/// We need to handle strings in a special way. They need to be sent down using CString and the
+/// pointer to it so have a generator for it
+///
+impl TypeHandlerTrait for StringTypeHandler {
+    fn replace_type(&self, arg: &Variable, is_ret_value: bool) -> Cow<str> {
+        match is_ret_value {
+            true => "String".into(),
+            false => "str".into(),
+        }
+    }
+
+    fn gen_body_return(&self, _varible: &Variable) -> Cow<str> {
+        "CStr::from_ptr(ret_val).to_string_lossy().into_owned()".into()
+    }
+
+    fn gen_body(&self, arg: &str, index: usize) -> (Cow<str>, Cow<str>) {
+        let arg_name = format!("str_in_{}_{}", arg, index);
+        (format!("{}.as_ptr()", arg_name).into(), 
+         format!("let {} = CString::new({}).unwrap();\n", arg_name, arg).into())
+    }
+}
 
 
 ///
@@ -64,30 +115,34 @@ fn generate_structs<W: Write>(f: &mut W, api_def: &ApiDef) -> io::Result<()> {
 ///
 /// Takes in a varibale and generates a Rust function variable argument
 ///
-fn generate_arg_type(dest: &mut String, var: &Variable) {
+fn generate_arg_type(dest: &mut String, var: &Variable, return_arg: bool, type_handler: &TypeHandler) {
     dest.clear();
+
+    // Run code to replace type if neeed
+    let type_name = {
+        match type_handler.get(var.type_name.as_str()) {
+            Some(handler) => handler.replace_type(var, return_arg),
+            None => var.type_name.clone().into(),
+        }
+    };
 
     match var.vtype {
         VariableType::None => dest.push_str("<None>"),
         VariableType::SelfType => dest.push_str("&self"),
-        VariableType::Primitive => dest.push_str(&var.type_name),
-        VariableType::Enum => dest.push_str(&var.type_name),
+        VariableType::Primitive => dest.push_str(&type_name),
+        VariableType::Enum => dest.push_str(&type_name),
         VariableType::Regular => {
             dest.push('&');
-            dest.push_str(&var.type_name)
+            dest.push_str(&type_name)
         },
         VariableType::Reference => {
             dest.push('&');
-            dest.push_str(&var.type_name)
+            dest.push_str(&type_name)
         },
         VariableType::Optional => {
-            if var.type_name == "String" {
-                dest.push_str("Option<&str>")
-            } else {
-                dest.push_str("Option<");
-                dest.push_str(&var.type_name);
-                dest.push('>');
-            }
+            dest.push_str("Option<");
+            dest.push_str(&type_name);
+            dest.push('>');
         }
     }
 }
@@ -95,14 +150,14 @@ fn generate_arg_type(dest: &mut String, var: &Variable) {
 ///
 /// Generate a function implementation
 ///
-fn generate_func_impl(func: &Function) -> String  {
+fn generate_func_impl(func: &Function, type_handler: &TypeHandler) -> String  {
     let mut temp_str = String::with_capacity(128);
 	let mut func_imp = String::with_capacity(128);
 
 	func_imp.push_str("(&self");
 
 	for arg in &func.function_args[1..] {
-        generate_arg_type(&mut temp_str, &arg);
+        generate_arg_type(&mut temp_str, &arg, false, type_handler);
 
         func_imp.push_str(", ");
         func_imp.push_str(&arg.name);
@@ -127,7 +182,7 @@ fn generate_func_impl(func: &Function) -> String  {
 	} else {
 	    let ret_val = func.return_val.as_ref().unwrap();
 
-        generate_arg_type(&mut temp_str, ret_val);
+        generate_arg_type(&mut temp_str, ret_val, true, type_handler);
 		func_imp.push_str(" -> ");
 
         if ret_val.array {
@@ -178,7 +233,7 @@ fn generate_structs_impl<W: Write>(f: &mut W, api_def: &ApiDef) -> io::Result<()
 ///
 ///
 ///
-fn generate_rute<W: Write>(f: &mut W, api_def: &ApiDef) -> io::Result<()> {
+fn generate_rute<W: Write>(f: &mut W, api_def: &ApiDef, type_handler: &TypeHandler) -> io::Result<()> {
     // write header
     f.write_all(RUTE_IMPL_HEADER)?;
 
@@ -212,10 +267,22 @@ fn generate_rute<W: Write>(f: &mut W, api_def: &ApiDef) -> io::Result<()> {
     f.write_all(b"}\n")
 }
 
+///
+/// Setup the type handlers
+///
+
+fn setup_type_handlers(_api_def: &ApiDef) -> TypeHandler {
+    let mut type_handler = TypeHandler::new();
+    type_handler.insert("String", Box::new(StringTypeHandler {}));
+    type_handler
+}
 
 impl RustGenerator {
     pub fn generate(filename: &str, api_def: &ApiDef) -> io::Result<()> {
         let mut f = BufWriter::new(File::create(filename)?);
+
+        // Setup type handlers
+        let type_handler = setup_type_handlers(api_def);
 
         // write header
         f.write_all(HEADER)?;
@@ -224,7 +291,7 @@ impl RustGenerator {
         generate_structs(&mut f, api_def)?;
 
         // Generate the main Rute entry
-        generate_rute(&mut f, api_def)?;
+        generate_rute(&mut f, api_def, &type_handler)?;
 
         Ok(())
     }
@@ -254,7 +321,7 @@ mod tests {
 	#[test]
 	fn test_function_self_only() {
         let func = build_default_func();
-		let func_impl = generate_func_impl(&func);
+		let func_impl = generate_func_impl(&func, &setup_type_handlers(&ApiDef::default()));
         assert_eq!(func_impl, "(&self) -> &Self<'a>");
 	}
 
@@ -266,8 +333,17 @@ mod tests {
         let mut func = build_default_func();
         func.function_args.push(Variable { name: "foo".to_owned(), type_name: "i32".to_owned(), vtype: VariableType::Primitive, .. Variable::default() });
 
-		let func_impl = generate_func_impl(&func);
+		let func_impl = generate_func_impl(&func, &setup_type_handlers(&ApiDef::default()));
         assert_eq!(func_impl, "(&self, foo: i32) -> &Self<'a>");
+	}
+
+	#[test]
+	fn test_function_string() {
+        let mut func = build_default_func();
+        func.function_args.push(Variable { name: "foo".to_owned(), type_name: "String".to_owned(), vtype: VariableType::Regular, .. Variable::default() });
+
+		let func_impl = generate_func_impl(&func, &setup_type_handlers(&ApiDef::default()));
+        assert_eq!(func_impl, "(&self, foo: &str) -> &Self<'a>");
 	}
 
 	//
@@ -279,7 +355,7 @@ mod tests {
         func.function_args.push(Variable { name: "width".to_owned(), type_name: "i32".to_owned(), vtype: VariableType::Primitive, .. Variable::default() });
         func.function_args.push(Variable { name: "height".to_owned(), type_name: "f32".to_owned(), vtype: VariableType::Primitive, .. Variable::default() });
 
-		let func_impl = generate_func_impl(&func);
+		let func_impl = generate_func_impl(&func, &setup_type_handlers(&ApiDef::default()));
         assert_eq!(func_impl, "(&self, width: i32, height: f32) -> &Self<'a>");
 	}
 
@@ -291,7 +367,7 @@ mod tests {
         let mut func = build_default_func();
         func.return_val = Some(Variable { type_name: "i32".to_owned(), vtype: VariableType::Primitive, .. Variable::default() });
 
-		let func_impl = generate_func_impl(&func);
+		let func_impl = generate_func_impl(&func, &setup_type_handlers(&ApiDef::default()));
         assert_eq!(func_impl, "(&self) -> i32");
 	}
 
@@ -303,7 +379,7 @@ mod tests {
         let mut func = build_default_func();
         func.return_val = Some(Variable { type_name: "f32".to_owned(), vtype: VariableType::Optional, .. Variable::default() });
 
-		let func_impl = generate_func_impl(&func);
+		let func_impl = generate_func_impl(&func, &setup_type_handlers(&ApiDef::default()));
         assert_eq!(func_impl, "(&self) -> Option<f32>");
 	}
 
@@ -315,7 +391,7 @@ mod tests {
         let mut func = build_default_func();
         func.return_val = Some(Variable { type_name: "f32".to_owned(), vtype: VariableType::Primitive, array: true, .. Variable::default()});
 
-		let func_impl = generate_func_impl(&func);
+		let func_impl = generate_func_impl(&func, &setup_type_handlers(&ApiDef::default()));
         assert_eq!(func_impl, "(&self) -> RefArray<f32, WrapperRcOwn>");
 	}
 
@@ -327,7 +403,7 @@ mod tests {
         let mut func = build_default_func();
         func.function_args.push(Variable { name: "width".to_owned(), type_name: "i32".to_owned(), vtype: VariableType::Primitive, array: true, .. Variable::default() });
 
-		let func_impl = generate_func_impl(&func);
+		let func_impl = generate_func_impl(&func, &setup_type_handlers(&ApiDef::default()));
         assert_eq!(func_impl, "(&self, width: &[i32]) -> &Self<'a>");
 	}
 }
